9407636a20444936936b336681e34d2fcb5c3cfd
Geovanne Silva <66743857+geovanne-s@users.noreply.github.com>
2026-02-12 18:46:55 -0300

    Configure CORS to allow all origins and methods
    Added CORS configuration to allow all origins and methods.

--- app/config/settings.py
+++ app/config/settings.py
@@ -7,4 +7,8 @@ ORCAMENTOS_DIR = BASE_DIR / "orcamentos"
 CORS_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    allow_origins=["*"], # Aceita qualquer domínio e qualquer porta
+    allow_credentials=False, # Precisa ser False se usar "*"
+    allow_methods=["*"],
+    allow_headers=["*"],
 ]
